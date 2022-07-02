8a43edb34f7fe073fed7d115883d128b57cb381b
Achmad Rif'an <achmadrifanlearn@gmail.com>
2022-07-02 19:57:26 +0700

    Update Tugas4_AchmadRifan.php

--- Tugas4_AchmadRifan.php
+++ Tugas4_AchmadRifan.php
@@ -6,22 +6,21 @@
     <title>Tugas4_AchmadRifan.php</title>
 </head>
 <body>
-<form action="" method="POST">
-    Jumlah Bintang = <input type="text" name="jumlahbintang" />
-    <br />
-    <input type="submit" name="submit" value="kirim">
-</form>
-<?php
+    <form action="" method="POST">
+        Jumlah Bintang = <input type="text" name="jumlahbintang" />
+        <br />
+        <input type="submit" name="submit" value="kirim">
+    </form>
+    <?php
     if(isset($_POST['submit'])) {
-    $bintang = $_POST['jumlahbintang'];
-    for($a = 1; $a <= $bintang; $a++) {
-        for($b = 1; $b <= $a; $b++) {
-        echo "*";
-    }
-    echo "<br />";
+        $bintang = $_POST['jumlahbintang'];
+        for($a = 1; $a <= $bintang; $a++) {
+            for($b = 1; $b <= $a; $b++) {
+                echo "*";
+            }
+            echo "<br />";
+        };
     };
-};
-?>
+    ?>
 </body>
 </html>
-
