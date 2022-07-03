9208e03c7997c9ab4acbcde7cfb4f7918432b71c
Achmad Rif'an <achmadrifanlearn@gmail.com>
2022-07-03 17:02:31 +0700

    Update Tugas5_AchmadRifan.php

--- Tugas5_AchmadRifan.php
+++ Tugas5_AchmadRifan.php
@@ -1,37 +1,87 @@
 <?php
-//fungsi untuk menampilkan bilangan
-function bilangan($bil1, $bil2) {
-	echo "bilangan 1 : ".$bil1."<br />";
-	echo "bilangan 2 : ".$bil2."<br />";
-}
-bilangan(9, 3); //memanggil fungsi bilangan
-echo "<hr><hr>";
+//mendeklarasikan variabel awal
+$bilangan1 = "";
+$bilangan2 = "";
+$tambah = "";
+$kurang = "";
+$kali = "";
+$bagi = "";
 
-//fungsi untuk melakukan penjumlahan
-function penjumlahan($plus1, $plus2) {
-	$hasiljumlah = $plus1 + $plus2; //penjumlahan dari argument plus1, plus2
-	echo "hasil penjumlahan adalah : " .$hasiljumlah. "<br />";
+//mengecek submit dan mendeklarasikan bilangan1 dan bilangan2 yang menerima data dari bil1 dan bil2
+if(isset($_POST['submit'])) {
+	$bilangan1 = $_POST["bil1"];
+	$bilangan2 = $_POST["bil2"];		
 }
-penjumlahan(9, 3); //memanggil fungsi penjumlahan
 
-//fungsi untuk melakukan pengurangan
-function pengurangan($min1, $min2){
-	$hasilpengurangan = $min1 - $min2; //pengurangan dari argument min1, min2
-	echo "hasil pengurangan adalah : " .$hasilpengurangan. "<br />";
+//mendeklarasikan variabel $tambah & memasukkan bilangan1 dan bilangan2 ke function TAMBAH
+if (isset($_POST['submit'])) {
+	$tambah = TAMBAH ($bilangan1, $bilangan2);
 }
-pengurangan(9, 3); //memanggil fungsi pengurangan
 
-//fungsi untuk melakukan perkalian
-function perkalian($kali1, $kali2) {
-	$hasilkali = $kali1 * $kali2; //perkalian dari argument kali1, kali2
-	echo "hasil perkalian adalah : " .$hasilkali. "<br />";
+//mendeklarasikan variabel $kurang & memasukkan bilangan1 dan bilangan2 ke function KURANG
+if (isset($_POST['submit'])) {
+	$kurang = KURANG ($bilangan1, $bilangan2);
 }
-perkalian(9, 3); //memanggil fungsi perkalian
 
-//fungsi untuk melakukan pembagian
-function pembagian($bagi1, $bagi2) {
-	$hasilbagi = $bagi1 / $bagi2; //pembagian dari argument bagi1, bagi2
-	echo "hasil pembagian adalah : " .$hasilbagi. "";
+//mendeklarasikan variabel $kali & memasukkan bilangan1 dan bilangan2 ke function KALI
+if (isset($_POST['submit'])) {
+	$kali = KALI ($bilangan1, $bilangan2);
 }
-pembagian(9, 3); //memanggil fungsi pembagian
+
+//mendeklarasikan variabel $bagi & memasukkan bilangan1 dan bilangan2 ke function BAGI
+if (isset($_POST['submit'])) {
+	$bagi = BAGI ($bilangan1, $bilangan2);
+}
+
+//funsi TAMBAH dengan mengembalikan bilangan1 + bilangan2
+function TAMBAH ($bilangan1, $bilangan2) {
+	return $bilangan1 + $bilangan2;
+}
+
+//funsi KURANG dengan mengembalikan bilangan1 + bilangan2
+function KURANG ($bilangan1, $bilangan2) {
+	return $bilangan1 - $bilangan2;
+}
+
+//funsi KALI dengan mengembalikan bilangan1 + bilangan2
+function KALI ($bilangan1, $bilangan2) {
+	return $bilangan1 * $bilangan2;
+}
+
+//funsi BAGI dengan mengembalikan bilangan1 + bilangan2
+function BAGI ($bilangan1, $bilangan2) {
+	return $bilangan1 / $bilangan2;
+}
 ?>
+
+</!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tugas5_AchmadRifan</title>
+</head>
+<body>
+	<div class="calculator">
+		<!--membuat form dengan method POST-->
+		<form action="" method="post">
+			Bilangan 1 : <input type="number" name="bil1" placeholder="Input 1st number"><br>
+			Bilangan 2 : <input type="number" name="bil2" placeholder="Input 2nd number"><br>
+			<input type="submit" name="submit" value="Submit" >
+		</form>
+		<!--menampilkan variabel bilangan1 dan bilangan2-->
+		bilangan 1 : <?php echo $bilangan1 ?> <br />
+		bilangan 2 : <?php echo $bilangan2 ?>
+		<hr>
+		<hr>
+		<!--menampilkan hasil penjumlahan dengan memanggil variabel $tambah-->
+		hasil penjumlahan adalah : <?php echo $tambah;?><br>
+		<!--menampilkan hasil pengurangan dengan memanggil variabel $kurang-->
+		hasil pengurangan adalah : <?php echo $kurang;?><br>
+		<!--menampilkan hasil perkalian dengan memanggil variabel $kali-->
+		hasil perkalian adalah : <?php echo $kali;?><br>
+		<!--menampilkan hasil pemabagian dengan memanggil variabel $bagi-->
+		hasil pembagian adalah : <?php echo $bagi;?>
+	</div>
+</body>
+</html>
