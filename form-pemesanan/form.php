efae55012b4423703ce7fd35a56dc5f85609ba4d
Achmad Rif'an <achmadrifanlearn@gmail.com>
2022-07-31 19:40:27 +0700

    Update form.php

--- form-pemesanan/form.php
+++ form-pemesanan/form.php
@@ -1,60 +1,65 @@
 <?php
-$namaanda = "";
-$identitasanda = "";
-$hpanda = "";
-$wisataanda = "";
-$padatanggal = "";
-$jumlahdewasa = "";
-$jumlahanak = "";
-$hitungbayar = "";
-$hargadewasa = [10000, 10000, 12500];
-$hargaanak = [5000, 10000, 10000];
-$kali1 = "";
-$kali2 = "";
-$hasilakhir = "";
+$namaAnda = "";
+$identitasAnda = "";
+$hpAnda = "";
+$daftarWisata = "";
+$padaTanggal = "";
+$jumlahDewasa = "";
+$jumlahAnak = "";
+$hargaTotal = "";
 
 if (isset($_POST['pesan'])) {
-    $namaanda = $_POST["nama"];
-    $identitasanda = $_POST["identitas"];
-    $hpanda = $_POST["hp"];
-    $wisataanda = $_POST["wisata"];
-    $padatanggal = $_POST["tanggal"];
-    $jumlahdewasa = $_POST["dewasa"];
-    $jumlahanak = $_POST["anak"];
-}
 
-if (isset($_POST['hitung'])) {
-    $jumlahdewasa = $_POST["dewasa"];
-    $jumlahanak = $_POST["anak"];
-    $hargadewasa = $_POST["wisata"];
-    $hargaanak = $_POST["wisata"];
-}
+    $tiket = array("Bukit Kayoe Putih" => 10000, "Pendakian Gunung Penanggungan" => 10000, "Air Terjun Dlundung" => 12000);
 
-if (isset($_POST['hitung'])) {
-    $kali1 = HITUNG1($jumlahdewasa, $hargadewasa);
-}
+    //Menghitung harga tiket awal
+    function hitungAwal($tiket, $daftarWisata)
+    {
+        $hargaAwal = $tiket[$daftarWisata];
+        return $hargaAwal;
+    }
 
-if (isset($_POST['hitung'])) {
-    $kali2 = HITUNG2($jumlahanak, $hargaanak);
-}
+    //Menghitung harga tiket dewasa
+    function hitungDewasa($tiket, $daftarWisata, $jumlahDewasa)
+    {
+        $hargaAwal = hitungAwal($tiket, $daftarWisata);
+        $hargaTiketDewasa = $hargaAwal * $jumlahDewasa;
+        return $hargaTiketDewasa;
+    }
 
-if (isset($_POST['hitung'])) {
-    $hasilakhir = HASIL($kali1, $kali2);
-}
+    //Menghitung harga diskon tiket anak
+    function hitungDiskonAnak($tiket, $daftarWisata)
+    {
+        $hargaAwal = hitungAwal($tiket, $daftarWisata);
+        $hargaDiskonAnak = $hargaAwal / 2;
+        return $hargaDiskonAnak;
+    }
 
-function HITUNG1($jumlahdewasa, $hargadewasa)
-{
-    return $jumlahdewasa * $hargadewasa;
-}
+    //Menghitung harga tiket anak
+    function hitungAnak($tiket, $daftarWisata, $jumlahAnak)
+    {
+        $hargaDiskonAnak = hitungDiskonAnak($tiket, $daftarWisata);
+        $hargaTiketAnak = $hargaDiskonAnak * $jumlahAnak;
+        return $hargaTiketAnak;
+    }
 
-function HITUNG2($jumlahanak, $hargaanak)
-{
-    return $jumlahanak * $hargaanak;
-}
+    //Menghitung total harga tiket
+    function hargaTotalTiket($hargaTiketDewasa, $hargaTiketAnak)
+    {
+        $hargaTotalTiket = $hargaTiketDewasa + $hargaTiketAnak;
+        return $hargaTotalTiket;
+    }
 
-function HASIL($kali1, $kali2)
-{
-    return $kali1 + $kali2;
+    $namaAnda = $_POST["nama"];
+    $identitasAnda = $_POST["identitas"];
+    $hpAnda = $_POST["hp"];
+    $daftarWisata = $_POST["daftarWisata"];
+    $padaTanggal = $_POST["tanggal"];
+    $jumlahDewasa = $_POST["jumlahDewasa"];
+    $jumlahAnak = $_POST["jumlahAnak"];
+    $hargaTiketDewasa = hitungDewasa($tiket, $daftarWisata, $jumlahDewasa);
+    $hargaTiketAnak = hitungAnak($tiket, $daftarWisata, $jumlahAnak);
+    $hargaTotal = hargaTotalTiket($hargaTiketDewasa, $hargaTiketAnak);
 }
 ?>
 
@@ -89,28 +94,28 @@ function HASIL($kali1, $kali2)
                 <tr>
                     <div class="input">
                         <td><label for="input-nama">Nama Lengkap</label></td>
-                        <td><input type="text" id="input-nama" name="nama"></td>
+                        <td><input type="text" id="input-nama" name="nama" required></td>
                         <td></td>
                     </div>
                 </tr>
                 <tr>
                     <div class="input">
                         <td><label for="input-identitas">Nomor Identitas</label></td>
-                        <td><input type="number" id="input-identitas" name="identitas"></td>
+                        <td><input type="number" id="input-identitas" name="identitas" required></td>
                         <td></td>
                     </div>
                 </tr>
                 <tr>
                     <div class="input">
                         <td><label for="input-hp">No. HP</label></td>
-                        <td><input type="number" id="input-hp" name="hp"></td>
+                        <td><input type="number" id="input-hp" name="hp" required></td>
                         <td></td>
                     </div>
                 </tr>
                 <tr>
                     <div class="dropdown">
                         <td><label for="dropdown-wisata">Pilih Tempat Wisata</label></td>
-                        <td><select name="wisata" id="dropdown-wisata">
+                        <td><select id="dropdown-wisata" name="daftarWisata" required>
                                 <option value="">Pilih Salah Satu Wisata</option>
                                 <option value="Bukit Kayoe Putih">Bukit Kayoe Putih</option>
                                 <option value="Pendakian Gunung Penanggungan">Pendakian Gunung Penanggungan</option>
@@ -122,14 +127,14 @@ function HASIL($kali1, $kali2)
                 <tr>
                     <div class="kunjungan">
                         <td><label for="input-tanggal">Tanggal Kunjungan</label></td>
-                        <td><input type="date" id="input-tanggal" name="tanggal"></td>
+                        <td><input type="date" id="input-tanggal" name="tanggal" required></td>
                         <td></td>
                     </div>
                 </tr>
                 <tr>
                     <div class="input">
                         <td><label for="input-dewasa">Pengunjung Dewasa</label></td>
-                        <td><input type="text" id="input-dewasa" name="dewasa"></td>
+                        <td><input type="number" id="input-dewasa" name="jumlahDewasa" required></td>
                         <td></td>
                     </div>
                 </tr>
@@ -138,7 +143,7 @@ function HASIL($kali1, $kali2)
                         <td><label for="input-anak">Pengunjung Anak-Anak</label>
                             <figcaption>Usia di bawah 12 tahun</figcaption>
                         </td>
-                        <td><input type="text" id="input-anak" name="anak" </td>
+                        <td><input type="number" id="input-anak" name="jumlahAnak" required></td>
                         <td></td>
                     </div>
                 </tr>
@@ -153,7 +158,7 @@ function HASIL($kali1, $kali2)
                     <td></td>
                 </tr>
                 <tr>
-                    <td colspan="3"><input type="checkbox" id="setuju">Saya dan/atau rombongan telah membaca, memahami, dan <br>setuju berdasarkan syarat dan ketentuan yang telah ditetapkan </td>
+                    <td colspan="3"><input type="checkbox" id="setuju" required>Saya dan/atau rombongan telah membaca, memahami, dan <br>setuju berdasarkan syarat dan ketentuan yang telah ditetapkan </td>
                 </tr>
                 <tr>
                     <td><input type="submit" id="hitung" value="Hitung Total Bayar" name="hitung"></td>
@@ -163,8 +168,9 @@ function HASIL($kali1, $kali2)
             </form>
         </table>
     </div>
+
     <div class="output">
-        <table border="1" cellpadding="6">
+        <table cellpadding="6">
             <tr>
                 <td colspan="2">
                     <div class="judul-bagform">Form Pemesanan</div>
@@ -172,35 +178,35 @@ function HASIL($kali1, $kali2)
             </tr>
             <tr>
                 <td>Nama Pemesan </td>
-                <td>: <?php echo $namaanda; ?></td>
+                <td>: <?php echo $namaAnda; ?></td>
             </tr>
             <tr>
                 <td>Nomor Identitas </td>
-                <td>: <?php echo $identitasanda; ?></td>
+                <td>: <?php echo $identitasAnda; ?></td>
             </tr>
             <tr>
                 <td>No. HP </td>
-                <td>: <?php echo $hpanda; ?></td>
+                <td>: <?php echo $hpAnda; ?></td>
             </tr>
             <tr>
                 <td>Tempat Wisata </td>
-                <td>: <?php echo $wisataanda; ?></td>
+                <td>: <?php echo $daftarWisata; ?></td>
             </tr>
             <tr>
                 <td>Tanggal Kunjungan </td>
-                <td>: <?php echo $padatanggal; ?></td>
+                <td>: <?php echo $padaTanggal; ?></td>
             </tr>
             <tr>
                 <td>Pengunjung Dewasa </td>
-                <td>: <?php echo $jumlahdewasa; ?></td>
+                <td>: <?php echo $jumlahDewasa; ?></td>
             </tr>
             <tr>
                 <td>Pengunjung Anak-Anak </td>
-                <td>: <?php echo $jumlahanak; ?></td>
+                <td>: <?php echo $jumlahAnak; ?></td>
             </tr>
             <tr>
                 <td>Harga Tiket</td>
-                <td>: <?php echo $hasilakhir; ?></td>
+                <td>: <?php echo $hargaTotal; ?></td>
             </tr>
         </table>
     </div>
